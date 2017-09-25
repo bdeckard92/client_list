f768a6af201247c971edfb3b00bf3a689724c7a8
Bob Deckard <bdeckard92@gmail.com>
2017-09-25 05:58:08 -0500

    deep clone

---
+++ deepClone.js
@@ -0,0 +1,5 @@
+var obj = {name: "Paddy", address: {town: "Lerum", country: "Sweden"}} -> {name: "Paddy", address: {town: "Lerum", country: "Sweden"}};
+
+function deepClone(obj){
+	clone = obj;
+};
